7770b2d469d72781d8c70ee8d8ec6a3d6c19140b
TheNerd <106362278+hehacker@users.noreply.github.com>
2022-07-20 19:31:12 -0400

    Add files via upload

---
+++ HowYouDoing.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "what is your name?"
+read name
+echo "good morning $name!!"
+sleep 1
+echo "you'er looking good today $name!!"
+sleep 1
+echo "what is your fav thing about your self NOTE only the word"
+read comliment
+echo "you have the best $comliment $name!!"
+#first script ez 
+
